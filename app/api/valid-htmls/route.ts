65d6e2d4fb503342839f9cbcb1fe4ab179352969
kdi0618 <65636371+kdi0618@users.noreply.github.com>
2023-11-21 07:32:24 +0700

    update: purge api cache
    fix: route config
    
    debug
    
    fix for debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix: debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix:debug
    
    fix: revalidate path
    
    update: purge api cache

--- app/api/valid-htmls/route.ts
+++ app/api/valid-htmls/route.ts
@@ -1,30 +1,73 @@
-import { revalidateTag } from 'next/cache';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { revalidateTag, revalidatePath } from 'next/cache';
+import { headers } from 'next/headers';
+import type { NextRequest } from 'next/server';
 
-export async function POST(req: any, res: NextApiResponse) {
-  console.log('start')
+import crypto from 'crypto';
+
+type Request = NextRequest & {
+  body: {
+    contents: {
+      old: {
+        id: string;
+      };
+      new: {
+        id: string;
+      };
+    };
+  };
+};
+
+export async function POST(request: Request) {
   try {
-    const crypto = require('crypto');
+    const headersList = headers();
+    const signature =
+      headersList.get('x-microcms-signature') || headersList.get('X-MICROCMS-Signature');
+
+    const requestJson = await request.json();
 
     const expectedSignature = crypto
-      .createHmac('sha256', 'remove11cache')
-      .update(req.body)
+      .createHmac('sha256', process.env.MICROCMS_WEBHOOK_SIGNATURE)
+      .update(JSON.stringify(requestJson))
       .digest('hex');
-    const signature = req.headers['X-MICROCMS-Signature'];
+
     if (
       !signature ||
       Array.isArray(signature) ||
       !crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expectedSignature))
     ) {
-      console.log('404stop')
-      return res.status(401).send('Invalid token');
+      return new Response('Invalid token', {
+        status: 401,
+      });
     }
 
-    const contentId = req.body.contents.new.id;
-    revalidateTag('blog')
-    console.log('revalidate', contentId);
-    return res.status(200).send('Revalidated successfully');
-  } catch (err) {
-    return res.status(500).send('Error revalidating');
+    console.log('Revalidation Start');
+    const contentId = requestJson.contents?.old?.id;
+
+    // fetchで取得したデータのキャッシュパージ
+    revalidateTag('blogData');
+
+    if (requestJson.api === 'blog') {
+      // 元から存在するページの場合、対象ページとTOPのキャッシュパージ
+      if (Boolean(requestJson.contents?.old?.id)) {
+        revalidatePath('/');
+        revalidatePath(`/articles/${contentId}`, 'page');
+      } else {
+        // 新規ページの場合はTOPのキャッシュパージ
+        revalidatePath('/');
+      }
+    } else if (requestJson.api === 'tags') {
+      // タグ変更の場合は全ページのキャッシュパージ
+      revalidatePath('/', 'layout');
+    }
+
+    console.log('Revalidation successful');
+    return new Response('Revalidation successful', {
+      status: 200,
+    });
+  } catch {
+    console.log('Revalidation Process Error');
+    return new Response('Revalidation failed', {
+      status: 500,
+    });
   }
 }
