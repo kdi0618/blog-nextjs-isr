4c223f9ba5f4d5c92b938ca9e1b402ebbda67581
Kodai Kudo <k.kodai0618@gmail.com>
2023-11-23 22:20:43 +0900

    fix:debug

--- app/api/valid-htmls/route.ts
+++ app/api/valid-htmls/route.ts
@@ -1,43 +1,44 @@
 import { revalidateTag } from 'next/cache';
+import { headers } from 'next/headers';
 import type { NextRequest } from 'next/server';
 
 import crypto from 'crypto';
 
-// type Request = NextRequest & {
-//   body: {
-//     contents: {
-//       new: {
-//         id: string;
-//       };
-//     };
-//   };
-// };
+type Request = NextRequest & {
+  body: {
+    contents: {
+      new: {
+        id: string;
+      };
+    };
+  };
+};
 
 export async function POST(request: any) {
   try {
+    const headersList = headers();
     const signature =
-      request.headers['x-microcms-signature'].value ||
-      request.headers['X-MICROCMS-Signature'].value;
+      headersList.get('x-microcms-signature') || headersList.get('X-MICROCMS-Signature');
     console.log('signature', signature);
 
-    console.log('request.body', request.body);
+    console.log('request.json', request.json());
 
     const expectedSignature = crypto
       .createHmac('sha256', 'remove11cache')
       .update(request.body)
       .digest('hex');
 
-    console.log('expectedSignature', expectedSignature);
+    // console.log('expectedSignature', expectedSignature);
 
-    if (
-      !signature ||
-      Array.isArray(signature) ||
-      !crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expectedSignature))
-    ) {
-      return new Response('Invalid token', {
-        status: 401,
-      });
-    }
+    // if (
+    //   !signature ||
+    //   Array.isArray(signature) ||
+    //   !crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expectedSignature))
+    // ) {
+    //   return new Response('Invalid token', {
+    //     status: 401,
+    //   });
+    // }
 
     console.log('req.body', request.body);
     console.log('req', request);
