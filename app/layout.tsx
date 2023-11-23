5d05d264a1a3229ccf551d1869c9241c8823ab3c
Kodai Kudo <k.kodai0618@gmail.com>
2023-11-24 02:35:33 +0900

    fix: add fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import './globals.css';
 import styles from './layout.module.css';
+import { Suspense } from 'react';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -32,8 +33,10 @@ export default async function RootLayout({ children }: Props) {
     <html lang="ja">
       <body>
         <Header />
-        <Nav tags={tags.contents} />
-        <main className={styles.main}>{children}</main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Nav tags={tags.contents} />
+          <main className={styles.main}>{children}</main>
+        </Suspense>
         <Footer />
       </body>
     </html>
